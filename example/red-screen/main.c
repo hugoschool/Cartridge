38504ec2ea5d85088b98ca15b11a5254b0b1b006
Hugo ARNAL <hugo@hugoarnal.com>
2026-05-19 14:46:20 +0200

    feat: entire menu main

--- example/red-screen/main.c
+++ example/red-screen/main.c
@@ -5,10 +5,24 @@
 int main(uint32_t CPSR, uint32_t IME)
 {
     uint32_t stack_addr = cpu_get_stack();
+    uint32_t rom_size = rom_get_size();
+    uint32_t code_size = code_get_size();
+    uint32_t const_size = const_get_size();
 
     dinit();
-    dclear(TFT_RED);
-    dprint_opt(120, 80, TFT_PURPLE, TFT_GREEN, 1, 1, "BAKAAAAAAAA %s", "HUGO");
+    dclear(TFT_BLACK);
+    dtext_opt(
+        SCREEN_WIDTH / 2, 12,
+        TFT_WHITE, TFT_BLACK,
+        DTEXT_HALIGN_MIDDLE, DTEXT_VALIGN_CENTER,
+        "Welcome!"
+    );
+    dprint_opt(1, 20, TFT_WHITE, TFT_BLACK, DTEXT_HALIGN_TOP, DTEXT_VALIGN_LEFT, "ROM: %d", rom_size);
+    dprint_opt(1, 32, TFT_WHITE, TFT_BLACK, DTEXT_HALIGN_TOP, DTEXT_VALIGN_LEFT, "Code: %d", code_size);
+    dprint_opt(1, 44, TFT_WHITE, TFT_BLACK, DTEXT_HALIGN_TOP, DTEXT_VALIGN_LEFT, "read-only data: %d", const_size);
+    dprint_opt(1, 56, TFT_WHITE, TFT_BLACK, DTEXT_HALIGN_TOP, DTEXT_VALIGN_LEFT, "stack: %p", stack_addr);
+    dprint_opt(1, 68, TFT_WHITE, TFT_BLACK, DTEXT_HALIGN_TOP, DTEXT_VALIGN_LEFT, "CPSR: %x", CPSR);
+    dprint_opt(1, 80, TFT_WHITE, TFT_BLACK, DTEXT_HALIGN_TOP, DTEXT_VALIGN_LEFT, "IME: %x", IME);
     while (1);
     return 0;
 }
